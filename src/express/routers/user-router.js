1732a82a1f0aec94456b599ada88f19bba4db582
reinert <daniloreinert@gmail.com>
2017-04-04 10:07:04 -0300

    routers proposal

--- src/express/routers/user-router.js
+++ src/express/routers/user-router.js
@@ -4,11 +4,18 @@ import { UserHandler } from '../handlers'
 const ID_PARAM = 'id'
 const ID_PATH = `/:${ID_PARAM}([0-9]+)`
 
-export default express.Router()
-    .get('/', UserHandler.getAll)
-    .post('/', UserHandler.retrievePassword, UserHandler.create)
-    .param(ID_PARAM, UserHandler.retrieveEntity)
-    .get(ID_PATH, UserHandler.getOne)
-    .patch(ID_PATH, UserHandler.retrievePassword, UserHandler.merge)
-    .put(ID_PATH, UserHandler.update)
-    .delete(ID_PATH, UserHandler.delete)
+const router = express.Router()
+
+router.param(ID_PARAM, UserHandler.retrieveEntity)
+
+router.route('/')
+  .get(UserHandler.getAll)
+  .post(UserHandler.retrievePassword, UserHandler.create)
+
+router.route(ID_PATH)
+  .get(UserHandler.getOne)
+  .patch(UserHandler.retrievePassword, UserHandler.merge)
+  .put(UserHandler.update)
+  .delete(UserHandler.delete)
+
+export default router
